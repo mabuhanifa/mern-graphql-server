7d3d1d8a1acfb0ddb3b8b29cd2bcdae1d59a3fe6
shourovahmd <moh.abuhanifa@gmail.com>
2023-02-22 04:11:59 +0600

    ClientType

--- schema/schema.js
+++ schema/schema.js
@@ -1,3 +1,15 @@
-const schema = '';
+const { GraphQLObjectType, GraphQLID, GraphQLString } = require("graphql");
+const { clients, projects } = require("../sampleData.js");
+
+const ClientType = new GraphQLObjectType({
+  name: "client",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+  }),
+});
+
 
 module.exports = schema;
