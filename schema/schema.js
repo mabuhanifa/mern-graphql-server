b963ad62b2441073c9f93ec7214ad91532e86284
shourovahmd <moh.abuhanifa@gmail.com>
2023-02-22 04:38:42 +0600

    ProjectType

--- schema/schema.js
+++ schema/schema.js
@@ -3,11 +3,24 @@ const {
   GraphQLID,
   GraphQLString,
   GraphQLSchema,
+  GraphQLInt,
+  GraphQLList,
 } = require("graphql");
 const { clients, projects } = require("../sampleData.js");
+//Client Type
+const ProjectType = new GraphQLObjectType({
+  name: "Project",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+  }),
+});
 
+//Client Type
 const ClientType = new GraphQLObjectType({
-  name: "client",
+  name: "Client",
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
@@ -19,6 +32,12 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve(parent, args) {
+        return clients;
+      },
+    },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
