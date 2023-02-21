8329c96878320e2619760c17fe214ba02d8922d3
shourovahmd <moh.abuhanifa@gmail.com>
2023-02-22 03:54:28 +0600

    schema

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
-
 const app = express();
+const schema = require("./schema/schema");
 app.use(express.json());
 app.use(cors());
+
+const { graphqlHTTP } = require("express-graphql");
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema
+  })
+);
 
 app.listen(port, () => {
   console.log(`App listening on ${port}`);
